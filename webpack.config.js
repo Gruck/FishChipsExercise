0d6f90242cf02c4bd8219db6d74ecc1b2516d8be
Gruck <gael.motte@gmail.com>
2016-11-20 12:43:23 +0100

    fixed webpack not refreshing html

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,15 @@
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+
 var path = require("path");
 module.exports = {
   entry: {
     app: ["./app/main.js"]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    })
+  ],
   output: {
     path: path.resolve(__dirname, "build"),
     publicPath: "/assets/",
